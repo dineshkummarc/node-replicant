af9fde220553fbb56fb8fb7eaf30adf0232b7695
James Halliday <mail@substack.net>
2012-01-05 17:17:18 -0800

    only write to stringify() if .pipe() is called

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var patcher = require('patcher');
-var Stream = require('stream');
 var JSONStream = require('JSONStream');
+var EventEmitter = require('events').EventEmitter;
 
 module.exports = function (obj) {
     var prev = patcher.clone(obj);
@@ -19,17 +19,28 @@ module.exports = function (obj) {
         function update (next) {
             var patch = patcher.computePatch(prev, next);
             prev = next;
-            stringify.write(patch);
+            self.emit('patch', patch);
         }
     };
     
-    var stringify = JSONStream.stringify();
     var parse = JSONStream.parse([ /./ ]);
     
     self.pipe = function (target) {
+        var stringify = JSONStream.stringify();
         stringify.pipe(target);
+        
+        self.on('patch', function (patch) {
+            stringify.write(patch);
+        });
         return self;
     };
+    
+    var emitter = new EventEmitter;
+    Object.keys(EventEmitter.prototype).forEach(function (key) {
+        if (typeof emitter[key] === 'function') {
+            self[key] = emitter[key].bind(emitter);
+        }
+    });
     
     return self;
 };
